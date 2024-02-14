40f557d281edb47a8fecabd989f16461de601a65
Ambrosio Hernández <ambrosio.rhc@gmail.com>
2024-02-14 23:40:27 +0000

    Corrigiendo links

--- src/js/app.js
+++ src/js/app.js
@@ -33,24 +33,22 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name !== null ? variables.name : "Name"} ${
-    variables.lastName !== null ? variables.lastName : "Lastname"
-  }</h1> 
+          <h1>${variables.name !== null ? variables.name : "Name"} 
+          ${variables.lastName !== null ? variables.lastName : "Lastname"}</h1> 
           <h2>${variables.role !== null ? variables.role : "Role"}</h2>
-          <h3>${variables.city !== null ? variables.city : "City"}, ${
-    variables.country !== null ? variables.country : "Country"
-  }</h3>
+          <h3>${variables.city !== null ? variables.city : "City"}, 
+          ${variables.country !== null ? variables.country : "Country"}</h3>
           <ul class=${variables.socialMediaPosition}>
-            <li><a href="https://${
+            <li><a href="https://twitter.com/${
               variables.twitter
             }"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://${
+            <li><a href="https://github.com/${
               variables.github
             }"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://${
+            <li><a href="https://www.linkedin.com/${
               variables.linkedin
             }"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://${
+            <li><a href="https://www.instagram.com/${
               variables.instagram
             }"><i class="fab fa-instagram"></i></a></li>
           </ul>
